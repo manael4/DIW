2226b215f9850f2ce803a90902c7e70b4913a7f7
manael4 <bmangel2004@gmail.com>
2024-12-04 21:39:13 +0100

    examen

---
+++ Examen/ExamenAnimaciones/script.js
@@ -0,0 +1,6 @@
+// El archivo js...
+// Así os ahorro el gran trabajo y sudor que supone crearlo y así Miguel está obligado a no integrarlo en el HTML
+function elem_visibles(){
+    let a = document.querySelector(".santa");
+    a.classList.toggle("a");
+}
